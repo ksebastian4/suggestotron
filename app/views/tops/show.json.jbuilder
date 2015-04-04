98edb32c99fab29af194c9348c43e6d6b8c9bf7b
Kathleen C. Sebastian <k.t.q.4.@sbcglobal.net>
2015-04-04 14:51:45 -0700

    Changed Gemfile for Heroku

---
+++ app/views/tops/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @top, :id, :title, :description, :created_at, :updated_at
---
+++ db/migrate/20150404202215_create_tops.rb
@@ -0,0 +1,10 @@
+class CreateTops < ActiveRecord::Migration
+  def change
+    create_table :tops do |t|
+      t.string :title
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
